379e86d556619837f6d7fbbaf19269bc14047c2a
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-14 08:39:13 -0300

    feat: logo search component

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 // import FollowersCard from '../FollowersCard/FollowersCard'
-// import LogoSearch from '../LogoSearch/LogoSearch'
+import LogoSearch from '../LogoSearch/LogoSearch'
 // import ProfileCard from '../ProfileCard.jsx/ProfileCard'
 
 import "./ProfileSide.css"
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
-        {/* <LogoSearch/>
-        <ProfileCard/>
-        <FollowersCard/> */}
+        <LogoSearch/>
+        {/* <ProfileCard/> */}
+        {/* <FollowersCard/> */}
     </div>
   )
 }
