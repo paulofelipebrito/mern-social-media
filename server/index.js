b5fe746df903897f838ca6ab45d32119fc36b524
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-15 08:55:34 -0300

    feat: handle post

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+// to serve images inside public folder
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
 dotenv.config();
 
 mongoose
