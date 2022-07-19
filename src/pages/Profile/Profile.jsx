f1307360eed1050135e2a04a65af0efe95fe4a38
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-19 08:53:27 -0300

    fix: filenames

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import PostSide from '../../components/PostSide/PostSide'
+import PostSide from '../../components/PostSide'
 import ProfileCard from '../../components/ProfileCard/ProfileCard'
 import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
-import RightSide from '../../components/RightSide/RightSide'
+import RightSide from '../../components/RightSide'
 import './Profile.css'
 const Profile = () => {
   return (
